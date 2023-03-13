989cec4c205f0f6637ebb928ef560c251b11251c
Batuhan <93447396+kumayy@users.noreply.github.com>
2023-03-14 01:18:08 +0300

    Add files via upload

---
+++ 29 Streamlit/02_import_df.py
@@ -0,0 +1,28 @@
+# core pkgs
+import streamlit as st
+
+# load EDA pkgs
+import pandas as pd
+
+# display data
+df = pd.read_csv("Module01/data/iris.csv")
+
+# Data display
+# st.dataframe(df,200,100)
+# st.dataframe(df) 
+# st.table(df) # static table
+# st.write(df)
+
+# Style
+st.dataframe(df.style.highlight_max(axis=0))
+
+# display Json
+st.json({"data" : "name"})
+
+# display code
+mycode="""
+def saymyname():
+    print("heisenberg")
+end
+"""
+st.code(mycode, language='python')
